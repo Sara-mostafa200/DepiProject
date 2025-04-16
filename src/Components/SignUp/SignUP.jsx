1c20a7a8dcd28067b12d33c035ed1e20524a67fd
Sara-mostafa200 <saramostafaelhadad830@gmail.com>
2025-04-16 11:39:14 +0200

    handlel error 6

--- src/Components/SignUp/SignUP.jsx
+++ src/Components/SignUp/SignUP.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import WelcomBack from "../../assets/WelcomeBack1.jpeg";
 
 export default function SignUP() {
     // hooks
     const navigate = useNavigate();
 
   return (
-    <main className="!bg-WelcomBack min-h-screen bg-cover bg-center flex justify-center items-center">
+    <main   style={{ backgroundImage: `url(${WelcomBack})` }}
+    className=" min-h-screen bg-cover bg-center flex justify-center items-center">
       <div className="bg-balckback text-white p-6 my-5 flex flex-col gap-16 rounded-md">
         {/* headline */}
         <h2 className=" text-[50px] font-bold text-center max-w-80">Create your 
